bddb3cd933b4e6dd43778c2bc966419b865e2138
Cindrella <cindrellapaul2001@gmail.com>
2023-08-05 12:51:04 +0400

    all the files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    // banner card owl carousel
     $('.banner-cards .owl-carousel').owlCarousel({
         autoplay: true,
         dots: true,
@@ -18,5 +19,39 @@ $(document).ready(function () {
             }
         }
     });
+
+    // about page brand logo slider
+    new Swiper('.clients-slider', {
+        speed: 400,
+        loop: true,
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false
+        },
+        slidesPerView: 'auto',
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'bullets',
+          clickable: true
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 40
+          },
+          480: {
+            slidesPerView: 3,
+            spaceBetween: 60
+          },
+          640: {
+            slidesPerView: 4,
+            spaceBetween: 80
+          },
+          992: {
+            slidesPerView: 6,
+            spaceBetween: 120
+          }
+        }
+      });
 
 });
