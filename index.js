64e468a788a5533701a5ee0458cf18b975892ce2
Cindrella <cindrellapaul2001@gmail.com>
2023-08-08 08:57:37 +0400

    all files

--- index.js
+++ index.js
@@ -53,5 +53,18 @@ $(document).ready(function () {
           }
         }
       });
+  
+  //policy section
+  $('body').addClass('js');
+  
+  var $menu = $('#side-menu'),
+    $menulink = $('.side-menu-link'),
+    $wrap = $('#wrap');
+  
+  $menulink.click(function() {
+    $menulink.toggleClass('active');
+    $wrap.toggleClass('active');
+    return false;
+  });
 
 });
